af3882a500ff947cbc32cf4f2cd853ce9d7856fa
Jorge Durán <ganchito55@gmail.com>
2016-11-10 22:36:57 +0100

    Agregar archivos de proyecto.

---
+++ Tareas/Tarea.cs
@@ -0,0 +1,16 @@
+﻿namespace Tareas
+{
+    public class Tarea
+    {
+        private static int contador = 0;
+        public int Numero { get; set; }
+        public string Info { get; set; }
+
+        public Tarea()
+        {
+            contador++;
+            Numero = contador;
+            Info = "Tarea" + contador;
+        }
+    }
+}
---
+++ Tareas/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tareas
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Tarea> Tareas { get; set; } = new ObservableCollection<Tarea>(); 
+    }
+}
