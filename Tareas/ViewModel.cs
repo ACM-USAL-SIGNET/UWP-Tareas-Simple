92d4c9e207ab6691d725977c33b053cb4f0c7347
Jorge Durán <ganchito55@gmail.com>
2016-11-12 17:26:05 +0100

    refactor

--- Tareas/ViewModel.cs
+++ Tareas/ViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
 namespace Tareas
 {
     public class ViewModel
     {
-        public ObservableCollection<Tarea> Tareas { get; set; } = new ObservableCollection<Tarea>(); 
+
+        // Sintaxis tipica de C#
+        // public ObservableCollection<Tarea> Tareas { get; set; } = new ObservableCollection<Tarea>();
+
+        public ObservableCollection<Tarea> Tareas { get; set; }
+
+        public ViewModel()
+        {
+            Tareas = new ObservableCollection<Tarea>();
+        }
     }
 }
